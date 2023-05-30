e9e5205cef989a447d1d1fb78eec9f26e3dd7412
Denis Upadyshev <telec1979@yandex.ru>
2023-05-30 21:32:06 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import types
+
+
+def flat_generator(list_of_lists):
+    new_list = []
+    count = 0
+    for lists in list_of_lists:
+        new_list.extend(lists)
+
+    while count < len(new_list):
+        count += 1
+        yield new_list[count-1]
+
+def test_2():
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_2()
